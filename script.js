3cf33d1e68b9234beeffef51beda8d43e4ad1a2a
IrinaK <s8obod@mail.ru>
2022-01-13 18:30:49 +0300

    delete, save

--- script.js
+++ script.js
@@ -1,23 +1,86 @@
-let container = document.querySelector('.items_block')
-const form =  document.getElementsByTagName('form')[0]
-const submit = document.getElementsByTagName('button')[0]
+function onPageLoaded() {
 
-submit.addEventListener('click', function addItem() {
-  const itemName = document.querySelector('#item_name').value
-  const itemDescription = document.querySelector('#item_description').value
-  const imgLink = document.querySelector('#img_link').value
-  const itemPrice = document.querySelector('#item_price').value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+  let container = document.querySelector('.items_block')
+  const form =  document.getElementsByTagName('form')[0]
+  const submit = document.getElementsByTagName('button')[0]
+  
+  function createItem() {
+    const itemName = document.querySelector('#item_name').value
+    const itemDescription = document.querySelector('#item_description').value
+    const imgLink = document.querySelector('#img_link').value
+    const itemPrice = document.querySelector('#item_price').value.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')
+    
+    const item = document.createElement("div")
+    item.classList.add("item_container")
 
-  container.insertAdjacentHTML("beforeend", 
-  `<div class="item_container">
-  <img src="${imgLink}">
-  <div class='description'>
-    <h4>${itemName}</h4>
-    <p> ${itemDescription}</p>
-    <p> ${itemPrice} руб. </p> 
-  </div>
-  </div>`)
-  form.reset()
-  submit.disabled = true
-})
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("deletebutton")
+    deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>'
+
+    const img = document.createElement("img")
+    img.setAttribute("src", imgLink)
+
+    const description = document.createElement("div")
+    description.classList.add("description")
+
+    const itemNameH = document.createElement('h4')
+    itemNameH.append(itemName)
+
+    const itemDescriptionP = document.createElement('p')
+    itemDescriptionP.append(itemDescription)
+
+    const itemPriceP = document.createElement('p')
+    itemPriceP.append(itemPrice)
+
+    description.append(itemNameH,itemDescriptionP, itemPriceP)
+
+    item.append(img, description, deleteBtn)
+
+    container.appendChild(item)
+
+    listenDeleteTodo(deleteBtn)
+    form.reset()
+    submit.disabled = true
+   
+
+    sessionStorage.setItem('items', container.innerHTML)
+
+    // container.insertAdjacentHTML("beforeend", 
+    //   `<div class="item_container">
+    //   <img src="${imgLink}">
+    //   <div class='description'>
+    //     <h4>${itemName}</h4>
+    //     <p> ${itemDescription}</p>
+    //     <p> ${itemPrice} руб. </p> 
+    //   </div>
+    //   </div>`)
+
+  }
+
+  function listenDeleteTodo(element) {
+    element.addEventListener("click", (event) => {
+        element.parentElement.remove();
+        event.stopPropagation();
+        sessionStorage.setItem('items', container.innerHTML)
+    });
+}
+
+function loadTodos() {
+  const data = sessionStorage.getItem("items");
+  if (data) {
+      container.innerHTML = data;
+  }
+  const deleteButtons = document.querySelectorAll(".deletebutton");
+  for (const button of deleteButtons) {
+      listenDeleteTodo(button);
+  }
+}
+
+loadTodos();
+
+  submit.addEventListener('click', () => createItem())
+
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded);
 
